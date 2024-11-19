190a367bf8049557b9977a975589d6fc382f96bd
Jexx2024 <lyubimov.evg@gmail.com>
2024-11-20 00:27:43 +0300

    Создан интерфейс,прописан код для установки картинки на метку.

---
+++ Cataas.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+def load_image(url):
+  pass
+
+window=Tk()
+window.geometry("500x500")
+
+label=Label()
+label.pack()
+url="https://cataas.com/cat"
+img=load_image(url)
+if img:
+    label.config(image=img)
+    #label.image=img
+
+    window.mainloop()

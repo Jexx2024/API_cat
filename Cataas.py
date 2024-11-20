7a8965d6eda3e242b5710378fb0c9569da8bbd94
Jexx2024 <lyubimov.evg@gmail.com>
2024-11-21 00:01:47 +0300

    Определена функия для отправки запроса и преобразования ответа в нужный формат.

--- Cataas.py
+++ Cataas.py
@@ -5,8 +5,13 @@ from io import BytesIO
 
 def load_image(url):
     response = requests.get(url)
-
-
+    print(response)
+    print(response.content)
+    image_data=BytesIO(response.content)
+    print(image_data)
+    img=Image.open(image_data)
+    print(img)
+    return ImageTk.PhotoImage(img)
 window=Tk()
 window.geometry("500x500")
 
@@ -16,6 +21,8 @@ url="https://cataas.com/cat"
 img=load_image(url)
 if img:
     label.config(image=img)
-    #label.image=img
 
-    window.mainloop()
+    #label.image=img
+but=Button(text="Загрузка картинки", command=load_image)
+but.pack()
+window.mainloop()
